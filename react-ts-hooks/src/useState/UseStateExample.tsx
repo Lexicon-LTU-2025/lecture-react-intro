aad804db0cfe5135b85f849da9e7dccab1925a87
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2025-08-21 09:21:15 +0200

    restructure examples

---
+++ react-ts-hooks/src/useState/UseStateExample.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState, type ReactElement } from 'react';
+import { Name } from './Name';
+
+export const UseStateExample = (): ReactElement => {
+  //let count = 10; // This doesn't work in React, the value is updated, but the DOM is not.
+  const [count, setCount] = useState(0);
+
+  const decremenet = (): void => {
+    setCount((prev) => prev - 1);
+  };
+
+  const increment = (): void => {
+    // count++; // This doesn't work in React, the value is updated, but the DOM is not.
+    // console.log(count);
+
+    setCount((prev) => prev + 1);
+  };
+
+  const reset = (): void => {
+    setCount(0);
+  };
+
+  // Ignore this for the moment. It just shows when the components i being rendered OR rerendered.
+  useEffect(() => {
+    console.log('App component was rendered');
+  });
+
+  return (
+    <main className="use-state-example">
+      <div className="counter">
+        <button onClick={decremenet}>Decrese count with 1</button>
+        <p>Count: {count}</p>
+        <button onClick={increment}>Increase count with 1</button>
+        <button onClick={reset}>Reset to 0</button>
+      </div>
+      <Name />
+    </main>
+  );
+};
